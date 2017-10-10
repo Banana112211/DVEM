0f9d8c87bf530b8e37c45fdfd64445b819361b30
Banana112211 <31136161+Banana112211@users.noreply.github.com>
2017-10-10 18:51:33 +0200

    Update

--- common_functions/UDP_Simulator.py
+++ common_functions/UDP_Simulator.py
@@ -5,14 +5,23 @@ Created on Fri Oct  6 11:01:13 2017
 @author: Gustav Willig
 """
 import socket
+import os
+import Write_Logfile
+
+#==============
 #todo: the received data need to be written to a log.txts
+#todo: received time stampe give to Video_Frame_recored
 UDP_IP = "192.168.0.104"
 UDP_PORT = 3333
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-
+Write_Logfile.logfile_schreiben("Current_time,WheelAngle,Accelrator,Brakepetal,SpeedInKilometers,PositionX,PositionY","Simulator")
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print ("received message:", data)
+    Write_Logfile.logfile_schreiben(data,"Simulator")
+     
+     
+     
