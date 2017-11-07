4f2ce05b5c9678f5c3044afd67ca4bb5e7f0b2e9
Banana112211 <31136161+Banana112211@users.noreply.github.com>
2017-11-07 09:13:49 +0000

    update

--- common_functions/server.py
+++ common_functions/server.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python           # This is server.py file
 
 import socket               # Import socket module
-
+import time
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
-s.bind(('141.7.25.167', 55604))        # Bind to the port
+s.bind(('192.168.0.1', 55605))        # Bind to the port
 
-s.listen(5)                 # Now wait for client connection.
+
 while True:
+   print("message1")
+   s.listen(5)                 # Now wait for client connection.
    c, addr = s.accept()     # Establish connection with client.
    print 'Got connection from', addr
-   c.send('Hello_Mario')
-   c.close()                # Close the connection
+   c.send(str(time.time()))
+   print("message2")
+c.close()                # Close the connection
