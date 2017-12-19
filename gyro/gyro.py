56984bbb3b6a1367eeb2c3c82c54f82d941a1d2a
Banana112211 <31136161+Banana112211@users.noreply.github.com>
2017-12-19 17:20:33 +0100

    releas gyro

--- gyro/gyro.py
+++ gyro/gyro.py
@@ -1,17 +1,27 @@
+ # -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 18:51:52 2017
+
+@author: home
+"""
+
 " The following code is based on https://github.com/withrobot/myAHRS_plus/tree/master/common_python/pygame/shooting/myahrs_plus.py"
 "Author: Gustav"
 
 import sys, time, thread, serial, traceback, os
 import numpy as np
+import writecsv2
+import find_gyro
 #=======#Change the working direction to "common_functions" and back
-dir_path = os.path.dirname(os.path.realpath(__file__)) #real current working direction
+"""dir_path = os.path.dirname(os.path.realpath(__file__)) #real current working direction
 dir_path_seperated=str(dir_path).split("/")# divide the current working direction
 common_functions=str(dir_path_seperated[0])+"/"+str(dir_path_seperated[1])+"/"+str(dir_path_seperated[2])+"/"+str(dir_path_seperated[3])+"/"+"common_functions"
 os.chdir(common_functions)
 #common load modul
 import Write_Logfile
 os.chdir(dir_path)
-#==============
+#=============="""
+
 class MyAhrsPlus(object):
     def __init__(self, serial_device="/dev/ttyACM0", baudrate=115200, *args, **kwargs):
         super(MyAhrsPlus, self).__init__(*args, **kwargs)
@@ -19,13 +29,13 @@ class MyAhrsPlus(object):
         try:
             serial_port = serial.Serial(serial_device, baudrate, timeout=0)
         except serial.serialutil.SerialException:
-            print 'Can not open serial port(%s)'%(serial_device)
+            print ('Can not open serial port(%s)'%(serial_device))
             serial_port = None 
             traceback.print_exc()
             return     
     
         self.serial_port = serial_port
-        
+       
         # Orientation 
         self.euler_angle_deg = (0, 0, 0)
         self.yaw_offset = 0
@@ -82,25 +92,25 @@ class MyAhrsPlus(object):
         # Get version 
         #
         rsp = self._send_command('version')
-        print rsp 
+        print (rsp) 
         
         #
         # Data transfer mode : ASCII, TRIGGER 
         #
         rsp = self._send_command('mode,AC')
-        print rsp  
+        print (rsp)  
         
         #
         # Select output message type 
         #
         rsp = self._send_command('asc_out,RPYIMU')
-        print rsp    
+        print (rsp)    
 
         #
         # speed 
         #
         rsp = self._send_command('divider,2')
-        print rsp   
+        print (rsp)   
 
         #
         # waiting for first data message 
@@ -112,7 +122,6 @@ class MyAhrsPlus(object):
         
     def read_euler(self):
         return self.euler_angle_deg
-
 
     def read_acceleration(self):
         return self.acceleration
@@ -136,7 +145,7 @@ class MyAhrsPlus(object):
         cmd_msg = cmd_msg + '*%02X'%crc + '\r\n'
         self.serial_port.write(cmd_msg)
         
-        print "%s"%(cmd_msg.strip())
+        print( "%s"%(cmd_msg.strip()))
         
         #
         # wait for response 
@@ -161,12 +170,11 @@ class MyAhrsPlus(object):
 
 
 
-def gyro_sensor():
-    if(len(sys.argv) < 2):
-        serial_device = '/dev/ttyACM0'
-    else : 
-        serial_device = sys.argv[1]
-    ahrs = MyAhrsPlus(serial_device=serial_device)
+def gyro_sensor(name=str(),queue=None):
+    name=name+"_gyr"
+    i=find_gyro.Find_Gyro_Input()
+    serial_device = '/dev/ttyACM'+str(i)
+    ahrs = MyAhrsPlus(serial_device)
         
     yaw_list = []
     for i in range(10):
@@ -175,21 +183,42 @@ def gyro_sensor():
     
     yaw_offset = np.mean(yaw_list)
     
-    print "Yaw offset %.2f"%(yaw_offset)
+    print( "Yaw offset %.2f"%(yaw_offset))
     
-    ahrs.set_yaw_offset(yaw_offset)
+    keys=['roll', 'pitch', 'yaw', 'accel_x', 'accel_y', 'accel_z', 'gyro_x', 'gyro_y', 'gyro_z','time','time_sim']
+    
+    g_data=dict([(key, []) for key in keys])
+    writecsv2.writedatacsv(name,g_data,0)
     nr=0
-    Write_Logfile.logfile_schreiben("roll, pitch, yaw,nr,time","gyro")
+    #Write_Logfile.logfile_schreiben("roll, pitch, yaw,nr,time","gyro")
     while(True):
-        now=time.asctime()
-        e = ahrs.read_euler();
-        print "EULER ANGLE (%.2f, %.2f, %.2f )"%e
-        message=str(str(e[0])+","+str(e[1])+","+str(e[2])+","+str(nr)+","+str(now)) #the whole contain must converted to a string!
-        Write_Logfile.logfile_schreiben(message,"gyro")
-        nr+=1
-        time.sleep(0.05)
+        try:
+            now=time.asctime()
+            e = ahrs.read_euler();
+            f=ahrs.read_acceleration()
+            g=ahrs.read_angular_rate()
+            for c in range(2):
+                g_data[keys[c]].append(e[c])
+                g_data[keys[c+3]].append(f[c])
+                g_data[keys[c+6]].append(g[c])
+            g_data[keys[9]].append(time.time())
+            if queue:
+                g_data[keys[10]].append(queue.get())
+            writecsv2.writedatacsv(name,g_data,1)
+                
+            print( "EULER ANGLE (%.2f, %.2f, %.2f )"%e)
+            message=str(str(e[0])+","+str(e[1])+","+str(e[2])+","+str(nr)+","+str(now)) #the whole contain must converted to a string!
+            #Write_Logfile.logfile_schreiben(message,"gyro")
+            
+            print(message)
+            nr+=1
+            time.sleep(0.05)
+        except KeyboardInterrupt:
+            break
+    
+    ahrs.serial_port.close()
 
 
 #=======Delete after successfull testing!
-gyro_sensor()
-    
+if __name__ == '__main__':
+    gyro_sensor("test")
